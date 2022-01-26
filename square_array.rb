600dde57674aff4d08a38d09d7e9a81f4aac7892
Hannah <hannahwoodward@hotmail.com>
2022-01-27 05:47:39 +0800

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do | element |
+    new_array << (element*element)
+  end
+  new_array # remember to return the new array otherwise will return the original
 end
